a4d5b5bb32fd8febbc487ef68570c7f849ef8473
youngyol <nasos9111@gmail.com>
2017-05-31 00:32:42 +0900

    Create 나머지.cpp

---
+++ 나머지.cpp
@@ -0,0 +1,26 @@
+// https://www.acmicpc.net/problem/10430
+
+#include <iostream>
+#include <string> 
+#include <deque>
+#include <algorithm>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+int main(int argc, char **argv) {
+	int a, b, c;
+	
+	cin >> a >> b >> c ;
+	if(a < 2 || a > 10000)	return 0;
+	if(b < 2 || b > 10000)	return 0;
+	if(c < 2 || c > 10000)	return 0;
+
+
+	cout << (a+b) % c << endl;
+	cout << (a%c + b%c) % c << endl;
+	cout << (a*b) % c << endl;
+	cout << ((a%c) * (b%c)) % c << endl;
+	
+}
+s
