f18813a49865efcb08acd4ab49f9fd1617531722
youngyol <nasos9111@gmail.com>
2017-06-09 16:04:47 +0900

    Create 가장_긴_증가하는_부분수열.cpp

---
+++ 가장_긴_증가하는_부분수열.cpp
@@ -0,0 +1,49 @@
+// https://www.acmicpc.net/problem/11053
+ 
+
+#include <iostream>
+#include <string> 
+#include <deque>
+#include <algorithm>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+int main(int argc, char **argv) {
+	int n;
+	vector <int> coin;
+	
+	vector <int> result;
+	
+	int tmp; 
+	int res = 0 ;
+	int fn = 0; 
+	
+	
+	cin >> n ;
+	if( n < 1 || n > 1000)	return 0;
+	
+	for(int i =0 ; i < n ; i ++ ){
+		cin >> tmp;
+		if( tmp < 1 || tmp > 1000)	return 0;
+		coin.push_back(tmp);
+		
+	}
+		
+	
+	int size = coin.size();
+	for(int i = 0 ; i < size ; i ++ ){
+		result.push_back(1);
+		for(int j = 0; j <i; j ++){
+			if(coin[j] <coin[i] && result[i] < result[j] + 1)
+				result[i] = result[j] + 1;			
+		}
+	}
+	int max=0;
+	size = result.size();
+	for(int i = 0 ; i < size ; i ++ ){
+		if(max < result[i]) max = result[i];
+	}
+			
+	cout << max;
+}
