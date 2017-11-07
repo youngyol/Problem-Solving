9fe97bac02d9910145ad9414a394c864858cb23f
youngyol <nasos9111@gmail.com>
2017-11-07 23:03:59 +0900

    Create 아스키코드.java

---
+++ 아스키코드.java
@@ -0,0 +1,12 @@
+// https://www.acmicpc.net/problem/11654
+
+import java.util.*;
+public class Main {
+ 
+
+	public static void main(String[] args){ 
+		Scanner sc = new Scanner(System.in);
+		System.out.printf("%d",(int)sc.next().toCharArray()[0]);
+	}
+ 
+}
