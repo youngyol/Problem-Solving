1a1bbd4aeb53183b4675c7f830e6aecd346bf56e
youngyol <nasos9111@gmail.com>
2017-11-14 17:59:58 +0900

    Update 하얀칸.java

--- 하얀칸.java
+++ 하얀칸.java
@@ -5,17 +5,16 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int numOnWhite = 0; 
-		int time = 1;
-		while(time <= 8){
-			int startSearch = (time%2 == 0)?1:0;
+		int numOnWhite = 0;
+		int time = 0;
+		while (time < 8) { 
 			char[] oneLine = sc.next().toCharArray();
-			for(int i = startSearch; i <8; i+=2){
-				numOnWhite += (oneLine[i]== 'F')?1:0;
+			for (int i = time % 2 ; i < 8; i += 2) {
+				numOnWhite += (oneLine[i] == 'F') ? 1 : 0;
 			}
 			time++;
 		}
 		System.out.println(numOnWhite);
-		
+
 	}
 }
