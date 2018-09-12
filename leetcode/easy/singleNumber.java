f27e9ddaf8fa1707f5e8685817439296bff0addf
youngyol <nasos9111@gmail.com>
2018-09-12 23:57:47 +0900

    Create singleNumber.java

---
+++ leetcode/easy/singleNumber.java
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/single-number/description/ 
+ 
+class Solution {
+    public int singleNumber(int[] nums) {
+        int x  = 0;
+        for(int num : nums)
+            x^= num;
+        return x;
+    }
+}
