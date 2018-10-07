625be1d615d8fe72711813e2b0c148f00858487b
youngyol <nasos9111@gmail.com>
2018-10-08 00:02:57 +0900

    Create ImplementQueueUsingStacks.java

---
+++ leetcode/easy/ImplementQueueUsingStacks.java
@@ -0,0 +1,67 @@
+// https://leetcode.com/problems/implement-queue-using-stacks/description/
+
+class MyQueue {
+    Stack<Integer> stack1;
+    Stack<Integer> stack2;
+    
+    /**
+     * Initialize your data structure here.
+     */
+    public MyQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    /**
+     * Push element x to the back of queue.
+     */
+    public void push(int x) {
+        stack1.push(x);
+    }
+
+    /**
+     * Removes the element from in front of queue and returns that element.
+     */
+    public int pop() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                int tmp = stack1.pop();
+                stack2.push(tmp);
+            }
+        }
+        return stack2.pop();
+
+    }
+
+    /**
+     * Get the front element.
+     */
+    public int peek() {
+        while (!stack1.isEmpty()) {
+            int tmp = stack1.pop();
+            stack2.push(tmp);
+        }
+        int ret = stack2.peek();
+        while(!stack2.isEmpty()){
+            int tmp = stack2.pop();
+            stack1.push(tmp); 
+        }
+        return ret;
+    }
+
+    /**
+     * Returns whether the queue is empty.
+     */
+    public boolean empty() {
+        return stack2.isEmpty() && stack1.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
