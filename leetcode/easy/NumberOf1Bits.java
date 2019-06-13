263a87c6ebf84d0dd16265ecb00599c78e44066b
youngyol <nasos9111@gmail.com>
2019-06-14 00:10:40 +0900

    Create NumberOf1Bits.java

---
+++ leetcode/easy/NumberOf1Bits.java
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/number-of-1-bits/
+
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int cnt = 0;
+        while(n!=0){
+            cnt += n&1;
+    		n = n>>>1;
+        }
+        return cnt;
+    }
+}
